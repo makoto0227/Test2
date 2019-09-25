df2afc551c2cedbe22cb77f212de2a2355ff9e36
miyazaki makoto <miyazakimakoto@miyazakimakotonoMacBook-Air.local>
2019-09-26 02:52:08 +0900

    first commit

---
+++ Test2/Test2/FirstViewController.swift
@@ -0,0 +1,44 @@
+//
+//  FirstViewController.swift
+//  Test2
+//
+//  Created by 宮崎真 on 2019/09/19.
+//  Copyright © 2019 宮崎真. All rights reserved.
+//
+
+import UIKit
+
+
+import PGFramework
+
+
+// MARK: - Property
+class FirstViewController: BaseViewController {
+
+    
+}
+
+// MARK: - Life cycle
+extension FirstViewController {
+    override func loadView() {
+        super.loadView()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+}
+
+// MARK: - Protocol
+extension FirstViewController {
+    
+}
+
+// MARK: - method
+extension FirstViewController {
+    
+}
---
+++ Test2/Test2/SecondViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SecondViewController.swift
+//  Test2
+//
+//  Created by 宮崎真 on 2019/09/20.
+//  Copyright © 2019 宮崎真. All rights reserved.
+//
+
+import UIKit
+
+
+import PGFramework
+
+
+// MARK: - Property
+class SecondViewController: BaseViewController {
+    
+}
+
+// MARK: - Life cycle
+extension SecondViewController {
+    override func loadView() {
+        super.loadView()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+}
+
+// MARK: - Protocol
+extension SecondViewController {
+    
+}
+
+// MARK: - method
+extension SecondViewController {
+    
+}
